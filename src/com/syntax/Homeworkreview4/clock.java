07b2191896505ca16b9a076d3dfc2109dba064d8
PeterFikry <peterfikry11@gmail.com>
2023-01-20 20:36:05 -0500

    homework

---
+++ src/com/syntax/Homeworkreview4/clock.java
@@ -0,0 +1,13 @@
+package com.syntax.Homeworkreview4;
+
+public class clock {
+    public static void main(String[] args) {
+
+        for (int i = 0; i <= 23; i++) {
+            for (int j = 0; j < 60; j++) {
+                System.out.println(i+":"+j);
+            }
+
+        }
+    }
+}
